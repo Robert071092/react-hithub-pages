da215d1b3f0649a378d2afb4f45bcac596045c2d
Robert071092 <114776835+Robert071092@users.noreply.github.com>
2024-02-08 21:43:24 -0600

    Add files via upload

---
+++ HOME/home.js
@@ -0,0 +1,26 @@
+import React , { Component } from "React";
+// Aqui importa el archivo Home.css
+import ´./home.css;
+
+class home extends Component {
+  render () {
+    return <h1>Welcome to Codejobs</h1>
+     <p> In this recipe you will learn how to add
+     styles to components. If you want to learn more you can visit our Youtube Channel at
+      <a href="http://youtube.com/codejobs"> Codejobs</a>.
+      </p
+      <p>
+      <button
+      style={{
+        backgroundColor: "gray"
+        border: "1px sol id black"
+      }}
+      >
+      Click me!
+      </button>
+      </p>
+      </div>
+    ) ;
+  }
+
+export default Home;
---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+const myFirstElement = <h1>Hello React hoooooy estoy aprendienbdo algo nuevo
+podriamos cambiaer el dom cambiar es facil el punto es adaptar la aplicaCION,
+esto es el dom !</h1>
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
+
+/*
+You are now watching
+the React file 'index.js'
+through our 'Show React' tool.
+*/
